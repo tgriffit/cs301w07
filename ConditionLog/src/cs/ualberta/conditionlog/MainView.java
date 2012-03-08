392dbab746735eac49b88c6361a064c2f9b540f1
adneufel <adneufel@ualberta.ca>
2012-03-07 20:33:35 -0700

    Committing a basic Android eclipse project.

---
+++ ConditionLog/src/cs/ualberta/conditionlog/MainView.java
@@ -0,0 +1,13 @@
+package cs.ualberta.conditionlog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainView extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
