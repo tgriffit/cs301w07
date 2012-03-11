1346fda68f7992f91abc585b15c66f74a46c1486
adneufel <adneufel@ualberta.ca>
2012-03-10 21:26:49 -0700

    A few minor cleanup edits

--- ConditionLog/src/cs/ualberta/conditionlog/MainView.java
+++ ConditionLog/src/cs/ualberta/conditionlog/MainView.java
@@ -2,11 +2,31 @@ package cs.ualberta.conditionlog;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainView extends Activity {
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button takePhotoButton = (Button) findViewById(R.id.TakePhotoButton);
+    	takePhotoButton.setOnClickListener(new View.OnClickListener() {
+
+    		public void onClick(View v) {
+    			// add button events here
+    		}
+    	});
+    	
+    	Button viewLogButton = (Button) findViewById(R.id.ViewLogsButton);
+    	viewLogButton.setOnClickListener(new View.OnClickListener() {
+
+    		public void onClick(View v) {
+    			// add button events here
+    		}
+    	});
     }
+	
 }
--- ConditionLog/src/cs/ualberta/conditionlog/PhotoList.java
+++ ConditionLog/src/cs/ualberta/conditionlog/PhotoList.java
@@ -1,17 +1,29 @@
 package cs.ualberta.conditionlog;
 
+import java.util.ArrayList;
+
 public class PhotoList {
 	private String name = "";
+	private ArrayList<String> filenames = null;
 	
 	PhotoList(String name) {
 		this.setName(name);
+		filenames = new ArrayList<String>();
 	}
 	
 	public String getName() {
 		return name;
 	}
-
+	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public void addPhoto(String filename) {
+		filenames.add(filename);
+	}
+	
+	public void sortPhotos() {
+		// implement after naming convention of photo timestamp is decided...
 	}
 }
