d9fb348a3d2eebe85d1e4fd6f72c895b0e8759e2
Tim Griffith <tgriffit@ualberta.ca>
2012-03-29 11:32:24 -0600

    Created testing class and moved appropriate files

---
+++ ConditionLog/src/cs/ualberta/conditionlog/controller/PasswordManager.java
@@ -0,0 +1,11 @@
+package cs.ualberta.conditionlog.controller;
+
+/**
+ * Provides methods for checking whether a password has been specified, creating a
+ * password, and testing whether a password is valid.
+ * @author tgriffit
+ *
+ */
+public class PasswordManager {
+
+}
---
+++ ConditionLog/src/cs/ualberta/conditionlog/model/EncryptionHelper.java
@@ -0,0 +1,5 @@
+package cs.ualberta.conditionlog.model;
+
+public class EncryptionHelper {
+
+}
