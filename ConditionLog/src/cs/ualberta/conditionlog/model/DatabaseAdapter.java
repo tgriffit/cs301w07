937ee376a9a33028668c3ea86ad6d8f98e6aada8
tgriffit <tgriffit@ualberta.ca>
2012-04-03 10:57:38 -0600

    Tags can now be removed from photos

--- ConditionLog/src/cs/ualberta/conditionlog/model/DatabaseAdapter.java
+++ ConditionLog/src/cs/ualberta/conditionlog/model/DatabaseAdapter.java
@@ -319,7 +319,7 @@ public class DatabaseAdapter {
      * @param cond
      */
     public void deleteConditionFromPhoto(String filename, String cond) {
-    	String[] args = {filename, cond};
+    	String[] args = {cond, filename};
     	
     	db.delete(DatabaseHelper.COND_TABLE, DatabaseHelper.COND_NAME + "=? AND " + DatabaseHelper.PHOTO_FILE + "=?", args);
     }
@@ -331,7 +331,7 @@ public class DatabaseAdapter {
      * @param tag
      */
     public void deleteTagFromPhoto(String filename, String tag) {
-    	String[] args = {filename, tag};
+    	String[] args = {tag, filename};
     	
     	db.delete(DatabaseHelper.TAGS_TABLE, DatabaseHelper.TAGS_NAME + "=? AND " + DatabaseHelper.PHOTO_FILE + "=?", args);
     }
